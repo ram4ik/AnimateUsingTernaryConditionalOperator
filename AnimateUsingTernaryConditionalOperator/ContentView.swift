d12bf0f4f6bb041c263f8a9c232cd105c0ad5db2
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-09-25 12:30:33 +0300

    init

--- AnimateUsingTernaryConditionalOperator/ContentView.swift
+++ AnimateUsingTernaryConditionalOperator/ContentView.swift
@@ -9,8 +9,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isRotating = false
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            Image(systemName: "bell")
+                .font(.largeTitle)
+            .scaleEffect(2)
+                .rotationEffect(.degrees(isRotating ? 360 : 0), anchor: .top)
+                .animation(Animation.spring(response: 0.87, dampingFraction: 0.5, blendDuration: 1).repeatForever(autoreverses: true))
+                .onAppear(){
+                    self.isRotating.toggle()
+                    
+            }
+            .onTapGesture {
+                self.isRotating.toggle()
+            }
+            Circle()
+                .frame(width: 20)
+                .shadow(radius: 5)
+                .offset(y: -30)
+            
+        }
     }
 }
 
